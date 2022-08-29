c72765b9d988959bc175ff40176b432487675204
Maximilien DI DIO <74690920+prx0@users.noreply.github.com>
2022-08-29 14:39:51 +0200

    Add the Close method for PgProxyServer

--- pgproxy.go
+++ pgproxy.go
@@ -6,11 +6,15 @@ import (
 	"log"
 	"net"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/jackc/pgproto3/v2"
 	"github.com/jackc/pgx/v4"
 )
 
+// Define the behavior you want during the session
+// by implementing the PgProxySession interface
 type PgProxySession interface {
 	OnConnect(socket *pgx.Conn)
 	OnQuery(query *pgproto3.Query) (*pgproto3.Query, error)
@@ -18,23 +22,23 @@ type PgProxySession interface {
 	OnClose(socket *pgx.Conn)
 }
 
-type PgProxyServerBackend struct {
+type pgProxyServerBackend struct {
 	backend *pgproto3.Backend
 	conn    net.Conn
 	db      *pgx.Conn
 }
 
-func NewPgProxyServerBackend(conn net.Conn, db *pgx.Conn) *PgProxyServerBackend {
+func newPgProxyServerBackend(conn net.Conn, db *pgx.Conn) *pgProxyServerBackend {
 	backend := pgproto3.NewBackend(pgproto3.NewChunkReader(conn), conn)
 
-	return &PgProxyServerBackend{
+	return &pgProxyServerBackend{
 		backend: backend,
 		conn:    conn,
 		db:      db,
 	}
 }
 
-func (p *PgProxyServerBackend) runQueryOnDB(query *pgproto3.Query) (pgx.Rows, error) {
+func (p *pgProxyServerBackend) runQueryOnDB(query *pgproto3.Query) (pgx.Rows, error) {
 	row, err := p.db.Query(context.Background(), query.String)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Query row failed: %v\n", err)
@@ -44,10 +48,10 @@ func (p *PgProxyServerBackend) runQueryOnDB(query *pgproto3.Query) (pgx.Rows, er
 	return row, nil
 }
 
-func (p *PgProxyServerBackend) Run(session PgProxySession) error {
+func (p *pgProxyServerBackend) run(session PgProxySession) error {
 	defer func() {
 		session.OnClose(p.db)
-		p.Close()
+		p.close()
 	}()
 
 	err := p.handleStartup()
@@ -110,7 +114,7 @@ func (p *PgProxyServerBackend) Run(session PgProxySession) error {
 	}
 }
 
-func (p *PgProxyServerBackend) handleStartup() error {
+func (p *pgProxyServerBackend) handleStartup() error {
 	startupMessage, err := p.backend.ReceiveStartupMessage()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error receiving startup message: %s", err)
@@ -140,16 +144,32 @@ func (p *PgProxyServerBackend) handleStartup() error {
 	return nil
 }
 
-func (p *PgProxyServerBackend) Close() error {
-	p.db.Close(context.Background())
-	return p.conn.Close()
+func (p *pgProxyServerBackend) close() error {
+	if p != nil && p.db != nil && !p.db.IsClosed() {
+		if err := p.db.Close(context.Background()); err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to close gracefully the database connection: %e", err)
+			return err
+		}
+	}
+
+	if p != nil && p.conn != nil {
+		if err := p.conn.Close(); err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to close gracefully the tcp server %e", err)
+			return err
+		}
+	}
+
+	return nil
 }
 
+// A PgProxyServer is a postgresql server proxy
 type PgProxyServer struct {
-	pgUrl   string
+	pgUri   string
 	session PgProxySession
+	backend *pgProxyServerBackend
 }
 
+// Listen TCP packets that use Message Flow postgresql protocol
 func (p *PgProxyServer) Listen(addr string) error {
 	listener, err := net.Listen("tcp", addr)
 	if err != nil {
@@ -166,16 +186,20 @@ func (p *PgProxyServer) Listen(addr string) error {
 		}
 		fmt.Println("Accepted connection from", conn.RemoteAddr())
 
-		db, err := pgx.Connect(context.Background(), p.pgUrl)
+		db, err := pgx.Connect(context.Background(), p.pgUri)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Unable to reach the database %s", err)
 		}
 
 		p.session.OnConnect(db)
 
-		b := NewPgProxyServerBackend(conn, db)
+		p.backend = newPgProxyServerBackend(conn, db)
+
+		sig_chan := make(chan os.Signal)
+		signal.Notify(sig_chan, os.Interrupt, syscall.SIGTERM)
+
 		go func() {
-			err := b.Run(p.session)
+			err := p.backend.run(p.session)
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "Something wrong occured with the backend %s", err)
 			}
@@ -184,6 +208,13 @@ func (p *PgProxyServer) Listen(addr string) error {
 	}
 }
 
-func CreatePgProxy(pgUrl string, session PgProxySession) *PgProxyServer {
-	return &PgProxyServer{pgUrl: pgUrl, session: session}
+// CreatePgProxy create a new proxy for a postgresql server
+// pgUri describe the postgresql URI for the postgresql server. See https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
+func CreatePgProxy(pgUri string, session PgProxySession) *PgProxyServer {
+	return &PgProxyServer{pgUri: pgUri, session: session, backend: nil}
+}
+
+// Close the PgProxyServer, close the database connection and the tcp server
+func (p *PgProxyServer) Close() error {
+	return p.backend.close()
 }
